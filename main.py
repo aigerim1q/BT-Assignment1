c1ea19fd94b3cd7ad61d785e78f6260fa1fe3491
NurasylKh <153474385+NurasylKh@users.noreply.github.com>
2024-12-21 20:49:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,20 @@ import hashlib
 def hash(text):
     hashed = hashlib.sha256(text.encode()).hexdigest()
     return hashed
+
+class MerkleTree:
+    def __init__(self, transactions):
+        self.transactions = transactions
+        self.root = self.build_tree(transactions)
+
+    def build_tree(self, transactions):
+        if len(transactions) == 1:
+            return hash(transactions[0])
+
+        new_level = []
+        for i in range(0, len(transactions), 2):
+            left = transactions[i]
+            right = transactions[i + 1] if i + 1 < len(transactions) else left
+            new_level.append(hash(left + right))
+
+        return self.build_tree(new_level)
